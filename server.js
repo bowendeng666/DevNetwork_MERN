0d67107af1259069611f6bf2dbb014e53cd17c58
bowendeng666 <bd45@rice.edu>
2021-08-20 23:18:35 -0500

    fix bugs

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = required('path');
+const path = require('path');
 // set up express server
 const app = express();
 
